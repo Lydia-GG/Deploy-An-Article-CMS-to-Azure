f34cd07654e4ff2d1b3f12a2d60fa644be0e36f6
Lydia Samir <L-Samir@hotmail.com>
2022-09-12 15:08:40 +0200

    added login message

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -75,8 +75,8 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        app.logger.info("You logged in successfully")
         next_page = request.args.get('next')
+        app.logger.warning("You logged in successfully")
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
